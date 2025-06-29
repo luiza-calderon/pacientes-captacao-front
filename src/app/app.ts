b1b925a5567f29d5247419f3cf49db51f3bd16ce
Vitor Nunes <vitor_nsilva@sicredi.com.br>
2025-06-29 19:34:58 -0300

    feat: Carrossel

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Header } from "./header/header";
+import { Carousel } from "./carousel/carousel";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [Header]
+  imports: [Header, Carousel]
 })
 export class App {
   //
