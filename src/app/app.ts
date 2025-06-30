af23a954bd36450f35fdf7fad9ad146a7ff41a5d
Vitor Nunes <vitor_nsilva@sicredi.com.br>
2025-06-30 15:13:07 -0300

    feat: Rodapé

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { Header } from "./header/header";
 import { Carousel } from "./carousel/carousel";
 import { Contact } from "./contact/contact";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [Header, Carousel, Contact]
+  imports: [Header, Carousel, Contact, Footer]
 })
 export class App {
   //
