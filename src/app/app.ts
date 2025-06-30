051f0bf328b5d178aafa38b17468f36c751b864f
Vitor Nunes <vitor_nsilva@sicredi.com.br>
2025-06-30 11:55:45 -0300

    feat: Formulário para contato

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { Header } from "./header/header";
 import { Carousel } from "./carousel/carousel";
+import { Contact } from "./contact/contact";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [Header, Carousel]
+  imports: [Header, Carousel, Contact]
 })
 export class App {
   //
