33818b22503077b4d5f44b105f9c74df506057aa
Vitor Nunes <vitor1nuness@gmail.com>
2025-07-08 14:37:12 -0300

    feat: Alterando componente carousel para banner

--- src/app/banner/banner.spec.ts
+++ src/app/banner/banner.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Carousel } from './carousel';
+import { Carousel } from './banner';
 
 describe('Carousel', () => {
   let component: Carousel;
