87839758104d37a40b00f063c6f52b70008eec20
Vitor Nunes <vitor1nuness@gmail.com>
2025-07-08 16:49:38 -0300

    feat: Adicionando sessão Sobre

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,12 +4,13 @@ import { Banner } from "./banner/banner";
 import { Services } from "./services/services";
 import { Contact } from "./contact/contact";
 import { Footer } from "./footer/footer";
+import { About } from "./about/about";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [Header, Banner, Services, Contact, Footer]
+  imports: [Header, Banner, Services, Contact, Footer, About]
 })
 export class App {
   //
